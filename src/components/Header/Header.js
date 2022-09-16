d8ee723edd0e50835ee5b67ab77f8cc9813c6fa5
Ados4453 <matthawkins44@gmail.com>
2022-09-15 22:36:36 -0500

    init for changes from the home page to get app to render

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import './Header.scss'
+import {NavLink} from "react-router-dom"
+
+function Header() {
+  return (
+    <div className='header'>
+        {/* <NavLink className='header_link'>
+            <img className='header_icon'>Icon</img>
+        </NavLink>
+        <NavLink className='header_link'>
+            <img className='header_icon'>Icon</img>
+        </NavLink>
+        <NavLink className='header_link'>
+            <img className='header_icon'>Icon</img>
+        </NavLink>
+        <NavLink className='header_link'>
+            <img className='header_icon'>Icon</img>
+        </NavLink>
+        <NavLink className='header_link'>
+            <img className='header_icon'>Icon</img>
+        </NavLink> */}
+    </div>
+  )
+}
+
+export default Header
