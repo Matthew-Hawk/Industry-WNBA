9d2b848d67f1be361009663ce2b95a9a0fc45cfd
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-16 02:39:16 -0400

    Add court analytics

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
 import Header from  "./components/Header/Header"
 import Artical from "./pages/Artical/Artical";
-
+import WatchLive from "./pages/WatchLive/WatchLive";
 
 function App() {
   
@@ -11,6 +11,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
+          <Route path="/watch-live" element={<WatchLive />} />
 					<Route path="/artical/:id" element={<Artical />} />
 				</Routes>
 
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,21 +1,10 @@
 import './Comments.scss';
-import heartIcon from '../../assets/icons/heart.svg';
-import shareIcon from '../../assets/icons/share.svg';
 
 const Comments = () => {
     return (
-        <section className="video-interactions">
-            <div className="comment-cta">
-                <button className="comment-cta__button">ADD COMMENT</button>
-                <div className="comment-cta__icons">
-                    <input className="comment-cta__like" type="image" src={heartIcon} alt="like"/>
-                    <input className="comment-cta__share" type="image" src={shareIcon} alt="share"/>
-                </div>
-            </div>
-            <div className="live-chat">
-                <h2 className="live-chat__title">LIVE CHAT</h2>
-            </div>
-        </section>
+        <div className="live-chat">
+            <h2 className="live-chat__title">LIVE CHAT</h2>
+        </div>
     )
 }
 
