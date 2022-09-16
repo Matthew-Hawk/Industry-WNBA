10ef5a8880d8fd77a3b69736cb58cd6470a39247
Graziela Pauselius <grazielak.pauselius@gmail.com>
2022-09-15 22:32:53 -0700

    Video settings

--- src/components/videoCard/VideoCard.js
+++ src/components/videoCard/VideoCard.js
@@ -10,6 +10,7 @@ const VideoCard = ({ videoData }) => {
 					title={videoData.title}
 					frameBorder="0"
 					allowFullScreen
+					allow="autoplay; encrypted-media"
 				/>
 				<h3 className="video__title">{videoData.title}</h3>
 			</div>
