3712641949e33e77f85b5327ea231a4110762872
Ados4453 <matthawkins44@gmail.com>
2022-09-16 02:56:57 -0500

    got the nav to link to the live video with respective icon

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,8 @@
 import './Nav.scss'
+
 import {NavLink} from "react-router-dom"
+import { useState } from 'react';
+
 import forum from '../../assets/icons/forum.svg'
 import home from '../../assets/icons/home.svg'
 import play from '../../assets/icons/play.svg'
@@ -8,15 +11,17 @@ import watch from "../../assets/icons/watch.svg"
 
 
 function Header() {
+    // const []
+
   return (
     <div className='header'>
-        <NavLink className='header__link'>
+        <NavLink className='header__link' to='/'>
             <img src={home}  alt='home' className='header__icon'></img>
         </NavLink>
         <NavLink className='header__link'>
             <img src={stats} alt='status' className='header__icon'></img>        
         </NavLink>
-        <NavLink className='header__link'>
+        <NavLink className='header__link' to="/watch-live">
             <img src={watch} alt='watch' className='header__icon'></img>
         </NavLink>
         <NavLink className='header__link'>
