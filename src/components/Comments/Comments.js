dd3a16c74f8d1000ec5cae43e6c254dcfcd819f9
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-16 00:33:17 -0400

    Add comment boilerplate

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,9 +1,11 @@
 import './Comments.scss';
+import heartIcon from '../../assets/icons/heart.svg';
 
 const Comments = () => {
     return (
         <section className="comments">
             <button className="comment-button">Add Comment</button>
+            <input type="image" src={heartIcon} alt="like"/>
         </section>
     )
 }
--- src/pages/WatchLive/WatchLive.js
+++ src/pages/WatchLive/WatchLive.js
@@ -1,12 +1,14 @@
 import './WatchLive.scss'
 import WatchHeader from '../../components/WatchHeader/WatchHeader';
 import videoData from '../../data/youtube-data.json';
+import Comments from '../../components/Comments/Comments';
 
 const VideoShare = () => {
     return (
         <>
             <WatchHeader />
             <video src={videoData[0].video}></video>
+            <Comments />
         </>
     )
 }
