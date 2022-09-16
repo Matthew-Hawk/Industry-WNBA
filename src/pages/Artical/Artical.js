52074e3e063225a69b239994328ca68973a932ac
Ados4453 <matthawkins44@gmail.com>
2022-09-16 00:16:12 -0500

    created artical js and scss file, linked them both, and created basic layout for js file

---
+++ src/pages/Artical/Artical.js
@@ -0,0 +1,14 @@
+import './Artical.scss'
+
+function Artical() {
+  return (
+    <>
+        <div className='parent'>
+            <div className='image-div'></div>
+            <section className='section-div'></section>
+        </div>
+    </>
+  )
+}
+
+export default Artical
