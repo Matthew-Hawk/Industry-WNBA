df9eff9df9178330766933738cef8506fa6bba1b
Graziela Pauselius <grazielak.pauselius@gmail.com>
2022-09-15 19:37:08 -0700

    homePage functionality

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import Articles from "../../components/articles/Articles";
+import data from "../../data/data.json";
 import "./HomePage";
 
-const HomePage = () => {};
+const HomePage = () => {
+	return (
+		<section>
+			<h1>Your Personalized Feed!</h1>
+			<div>
+				{data.map((articleData) => (
+					<Articles articleData={articleData} />
+				))}
+			</div>
+		</section>
+	);
+};
 
 export default HomePage;
